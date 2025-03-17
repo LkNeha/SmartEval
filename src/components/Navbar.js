1b5749b0794997725f21ac05b463b3d770f29e1f
Neha lk <lkneha123@gmail.com>
2025-03-17 15:12:54 +0100

    css upadtes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,10 @@
 import React, { useState } from 'react'
+import { FaFacebook, FaInstagram, FaPinterest, FaTwitter, FaYoutube } from 'react-icons/fa'
+import { BiSearch } from 'react-icons/bi'
+import { BsPerson } from 'react-icons/bs'
+import { HiOutlineMenuAlt4 } from 'react-icons/hi'
+import { AiOutlineClose } from 'react-icons/ai'
+import { Link } from 'react-router-dom'
 import './Navbar.css'
 
 
@@ -7,54 +13,54 @@ function Navbar() {
     const handleNav = () => setNav(!nav)
 
     return (
-        // <div name='home' className={nav ? 'navbar navbar-bg' : 'navbar'}>
-        //     <div className="logoImg">
-        //         <img src={final} alt=''/>
+        <div name='home' className={nav ? 'navbar navbar-bg' : 'navbar'}>
+            {/* <div className="logoImg">
+                <img src={} alt=''/>
                
-        //     </div>
-        //     <ul className="nav-menu">
-        //         <Link to="/"><li>Home</li></Link>
-        //         <Link to="/teacher"><li>Teachers</li></Link>
-        //         <Link to="/student"><li>Students</li></Link>
-        //         <Link to="/teacher/viewScore"><li>Scores</li></Link>
-        //     </ul>
+            </div> */}
+            <ul className="nav-menu">
+                <Link to="/"><li>Home</li></Link>
+                <Link to="/teacher"><li>Teachers</li></Link>
+                <Link to="/student"><li>Students</li></Link>
+                <Link to="/teacher/viewScore"><li>Scores</li></Link>
+            </ul>
             
-        //     <div className="nav-icons">
+            <div className="nav-icons">
                 
-        //         <BiSearch className='icon' style={{ marginRight: '1rem' }} />
-        //         <BsPerson className='icon' />
-        //     </div>
-        //     <div className="hamburger" onClick={handleNav}>
-        //         {!nav ? (<HiOutlineMenuAlt4 className='icon' />) : (<AiOutlineClose style={{ color: '#000' }} className='icon' />)}
+                <BiSearch className='icon' style={{ marginRight: '1rem' }} />
+                <BsPerson className='icon' />
+            </div>
+            <div className="hamburger" onClick={handleNav}>
+                {!nav ? (<HiOutlineMenuAlt4 className='icon' />) : (<AiOutlineClose style={{ color: '#000' }} className='icon' />)}
 
-        //     </div>
+            </div>
 
-        //     <div className={nav ? 'mobile-menu active' : 'mobile-menu'}>
-        //         <ul className="mobile-nav">
-        //          <Link to="/"><li>Home</li></Link>
-        //             <Link to="/teacher"><li>Teachers</li></Link>
-        //             <Link to="/student"><li>Students</li></Link>
-        //             <Link to="/viewScore"><li>Scores</li></Link>
-        //         </ul>
-        //         <div className="mobile-menu-bottom">
-        //             <div className="menu-icons">
-        //                 <button>Search</button>
-        //                 <button>Account</button>
-        //             </div>
-        //             <div className="social-icons">
-        //                 <FaFacebook className='icon' />
-        //                 <FaInstagram className='icon' />
-        //                 <FaTwitter className='icon' />
-        //                 <FaPinterest className='icon' />
-        //                 <FaYoutube className='icon' />
-        //             </div>
-        //         </div>
-        //     </div>
-
-        // </div>
-        <div>
+            <div className={nav ? 'mobile-menu active' : 'mobile-menu'}>
+                <ul className="mobile-nav">
+                 <Link to="/"><li>Home</li></Link>
+                    <Link to="/teacher"><li>Teachers</li></Link>
+                    <Link to="/student"><li>Students</li></Link>
+                    <Link to="/viewScore"><li>Scores</li></Link>
+                </ul>
+                <div className="mobile-menu-bottom">
+                    <div className="menu-icons">
+                        <button>Search</button>
+                        <button>Account</button>
+                    </div>
+                    {/* <div className="social-icons">
+                        <FaFacebook className='icon' />
+                        <FaInstagram className='icon' />
+                        <FaTwitter className='icon' />
+                        <FaPinterest className='icon' />
+                        <FaYoutube className='icon' />
+                    </div> */}
+                </div>
+            </div>
 
         </div>
+        // <div>
+
+        // </div>
     )
 }
 
